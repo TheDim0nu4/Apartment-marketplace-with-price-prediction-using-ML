5c3aedc6dad19a7bfcccd708fe8be45e6a175374
TheDim0nu4 <dmitroskripcenko@gmail.com>
2026-04-05 16:19:41 +0300

    created api for price prediction

---
+++ ML/api/main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, HTTPException
+import joblib
+from pydantic import BaseModel, Field
+import pandas as pd
+
+
+
+
+app = FastAPI()
+
+
+
+
+model = joblib.load('xgboost.joblib')
+
+
+
+
+class Data(BaseModel):
+    rooms: int = Field(..., gt=0)
+    size: float = Field(..., gt=0)
+    reconstructed: str
+    garage: str
+    balcony: str
+    new: str
+    location: str
+
+
+
+
+@app.get('/')
+def index():
+    return {'status': 'everything is okay'}
+
+
+
+
+
+@app.post('/get-apartment-price-prediction')
+def get_apartment_price_prediction(data: Data):
+
+    data = data.model_dump() 
+
+    for key in ['reconstructed', 'garage', 'balcony', 'new']:
+
+        if data[key] == 'Yes':
+            data[key] = 1
+        elif data[key] == 'No':
+            data[key] = 0
+        else:
+            raise HTTPException(status_code=400, detail=f"The attribute '{key}' can be 'Yes' or 'No'.")
+        
+    
+    data = {key:[value] for key, value in data.items()}
+    data = pd.DataFrame(data)
+
+    prediction = model.predict(data)
+    prediction = float(prediction[0])
+
+
+    return {'predicted price': prediction}
+
